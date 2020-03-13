ceaf00ae5608e40a685f06a2c67c54c121fc6c33
王垚 <11344332@qq.com>
2020-03-13 16:12:03 +0800

    Version 1.0.2

--- keras_ernie/__init__.py
+++ keras_ernie/__init__.py
@@ -5,9 +5,9 @@
 @Author: Wang Yao
 @Date: 2020-03-13 13:53:26
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-13 16:09:20
+@LastEditTime: 2020-03-13 16:11:45
 '''
 from .loader import *
 from .paddle_to_tensor import *
 
-__version__ = '1.10.1'
+__version__ = '1.0.2'
