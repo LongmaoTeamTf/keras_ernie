946a49bda45d98b60ee4e046de6d46592cfef118
王垚 <11344332@qq.com>
2020-03-13 18:47:54 +0800

    Fix

--- keras_ernie/__init__.py
+++ keras_ernie/__init__.py
@@ -5,9 +5,9 @@
 @Author: Wang Yao
 @Date: 2020-03-13 13:53:26
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-13 16:30:07
+@LastEditTime: 2020-03-13 18:47:38
 '''
 from .loader import *
-from .paddle_to_tensor import *
+from .convert import *
 
 __version__ = '1.0.4'
