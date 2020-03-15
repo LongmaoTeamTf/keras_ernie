a67358e8f728dfffae369b8388e6fc118d025c96
王垚 <11344332@qq.com>
2020-03-15 18:17:03 +0800

    Version 1.0.5

--- keras_ernie/__init__.py
+++ keras_ernie/__init__.py
@@ -5,9 +5,9 @@
 @Author: Wang Yao
 @Date: 2020-03-13 13:53:26
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-13 18:47:38
+@LastEditTime: 2020-03-15 18:16:50
 '''
 from .loader import *
 from .convert import *
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
