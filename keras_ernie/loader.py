7e483a3e60b2acf76772c599ab517dd53f86bae8
王垚 <11344332@qq.com>
2020-03-15 18:49:10 +0800

    Fix loader

--- keras_ernie/loader.py
+++ keras_ernie/loader.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-03-12 19:00:08
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-15 18:41:28
+@LastEditTime: 2020-03-15 18:48:54
 '''
 import os
 import tensorflow as tf
@@ -51,14 +51,3 @@ def load_from_checkpoint(init_checkpoint, ernie_config_path, ernie_vocab_path, e
     model.name = name
     
     return model
-
-
-if __name__ == "__main__":
-    ernie_path = "/media/xddz/xddz/data/ERNIE_stable-1.0.1"
-    init_checkpoint = os.path.join(ernie_path, 'params')
-    ernie_config_path = os.path.join(ernie_path, 'ernie_config.json')
-    ernie_vocab_path = os.path.join(ernie_path, 'vocab.txt')
-    ernie_version = "stable-1.0.1"
-    model = load_from_checkpoint(
-        init_checkpoint, ernie_config_path, ernie_vocab_path, ernie_version,)
-    model.summary()
