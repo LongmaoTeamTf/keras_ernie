b44cb712ebc98f0fbb43ecaefab6ffe139139927
王垚 <11344332@qq.com>
2020-03-15 18:43:59 +0800

    Fix demo

--- demo/test_convert.py
+++ demo/test_convert.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-03-15 18:26:33
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-15 18:27:00
+@LastEditTime: 2020-03-15 18:42:53
 '''
 import os
 from keras_ernie import ErnieArgs
@@ -18,7 +18,6 @@ ernie_vocab_path = os.path.join(ernie_path, 'vocab.txt')
 tensor_checkpoints_dir = "/root/checkpoints"
 
 args = ErnieArgs(init_checkpoint, ernie_config_path, ernie_vocab_path,
-        max_seq_len=128, num_labels=2, use_fp16=False,
-        use_gpu=True, gpu_memory_growth=False)
+        max_seq_len=128, num_labels=2, use_fp16=False)
 
 convert_paddle_to_tensor(args, tensor_checkpoints_dir)
--- demo/test_load_ernie.py
+++ demo/test_load_ernie.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-03-12 22:46:00
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-13 00:51:11
+@LastEditTime: 2020-03-15 18:43:03
 '''
 import os
 from keras_ernie import load_from_checkpoint
@@ -18,6 +18,5 @@ ernie_vocab_path = os.path.join(ernie_path, 'vocab.txt')
 ernie_version = "stable-1.0.1"
 
 model = load_from_checkpoint(init_checkpoint, ernie_config_path, ernie_vocab_path, ernie_version,
-            max_seq_len=128, num_labels=2, use_fp16=False, use_gpu=True, gpu_memory_growth=False,
-            training=False, seq_len=None, name='ernie')
+            max_seq_len=128, num_labels=2, use_fp16=False, training=False, seq_len=None, name='ernie')
 model.summary()
